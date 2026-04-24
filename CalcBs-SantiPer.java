a4e7683ea4f8605c45b7f505216b85f343313ab8
Santiago Per <persantiago3@gmail.com>
2026-04-23 23:01:11 -0600

    Add files via upload

---
+++ CalcBs-SantiPer.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadorabasica27anggelorosales;
+
+/**
+ *
+ * @author Pamela
+ */
+public class CalculadoraBasica27AnggeloRosales {
+
+    public static void main(String[] args) {
+          jfrmCalc holapos=new jfrmCalc ();
+            holapos.setVisible(true);
+    }
+}
